d6b269c6766ebaf6544910199bf1f66a490a230d
Luis Pg <luispy99@gmail.com>
2024-11-18 19:24:11 +0100

    updated main

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     pygame.init()
     print("Starting asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -12,6 +15,7 @@ def main():
                 return
         screen.fill(0)
         pygame.display.flip()
+        dt = pygame.time.Clock().tick(60) / 1000
     
 
 if __name__ == "__main__":
